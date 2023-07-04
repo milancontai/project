02fdcab5343f3f296e71686035a627036b8af5ee
milancontai <mscontai2002@gmail.com>
2023-07-04 19:43:54 +0100

    first commit

---
+++ A.java
@@ -0,0 +1,13 @@
+class A
+{
+  public static void main(String args[])
+  {
+    System.out.println("Parameter Recived");
+    System.out.println(args[0]);
+    System.out.println(args[1]);
+    System.out.println(args[2]);
+    System.out.println(args[3]);
+    System.out.println(args[4]);
+    System.out.println(args[5]);
+   }
+}
